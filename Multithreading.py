841a16f8011151faae6d72dbfdc1edaf165cd31f
himitsuri <shakirov1990@gmail.com>
2015-10-24 21:26:26 +0300

    Last

--- Multithreading.py
+++ Multithreading.py
@@ -15,28 +15,28 @@ def get_from_queue():
         print ('Число : %s\t' % (item.get()))
 
 def put_to_queue():
-    for x in range(100000000): item.put(x)
+    for x in range(1000000000): item.put(x)
 
 p0 = threading.Thread(target=put_to_queue, name="t0")
 p1 = threading.Thread(target=get_from_queue, name="t1")
 p2 = threading.Thread(target=get_from_queue, name="t2")
 p3 = threading.Thread(target=get_from_queue, name="t3")
-#p4 = threading.Thread(target=get_from_queue, name="t4")
+p4 = threading.Thread(target=get_from_queue, name="t4")
 
 #Запуск потоков в качестве демонов
-#p1.setDaemon(True)
-#p2.setDaemon(True)
-#p3.setDaemon(True)
-#p4.setDaemon(True)
+p1.setDaemon(True)
+p2.setDaemon(True)
+p3.setDaemon(True)
+p4.setDaemon(True)
 
 p0.start()
 p1.start()
 p2.start()
 p3.start()
-#p4.start()
+p4.start()
 
 #Ожидание прерывания потоков
-#p1.join()
-#p2.join()
-#p3.join()
-#p4.join()
+p1.join()
+p2.join()
+p3.join()
+p4.join()
